01d8f6f0132ba2f899ba7752455dfa5bd5f24a85
Guru Prakash Kumar <guruprakash745@gmail.com>
2024-02-27 12:39:58 +0530

    minor change

--- lib/src/config/const.dart
+++ lib/src/config/const.dart
@@ -6,9 +6,9 @@ class Const {
   // static const apiUrl = 'https://e484-2405-201-6802-d99c-799e-a09c-8008-42e8.ngrok-free.app';
   // static const apiUrl = 'https://8702-2409-40e1-1072-a7a3-4854-9f6e-40bf-acc5.ngrok-free.app';
 
-  // static const apiUrl = 'https://cube-backend-combined-1.onrender.com';
+  static const apiUrl = 'https://cube-backend-combined-1.onrender.com';
 
-  static const apiUrl = 'http://192.168.226.119:8001';
+  // static const apiUrl = 'http://192.168.226.119:8001';
 
   static const apiV1Url = apiUrl + '/api/v1';
 }
